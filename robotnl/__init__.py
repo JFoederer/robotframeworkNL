0683f41e29b1b1e072ac3aea431899860e60ad6e
J. Foederer <32476108+JFoederer@users.noreply.github.com>
2021-02-13 16:56:30 +0100

    Make library directly importable as robotnl

--- robotnl/__init__.py
+++ robotnl/__init__.py
@@ -1,3 +1,10 @@
 from .version import VERSION
+from .RobotChecks import RobotChecks
+from .CheckOperator import CheckOperator
+
+class robotnl(RobotChecks, CheckOperator):
+    """
+    Generic non-domain specific keywords for use by all to enhance the Natural Language experience
+    """
 
 __version__ = VERSION
--- robotnl/natural_language.robot
+++
@@ -1,4 +0,0 @@
-*** Settings ***
-Documentation     Generic non-domain specific keywords for use by all to enhance the Natural Language experience
-Library           RobotChecks.py
-Library           CheckOperator.py
